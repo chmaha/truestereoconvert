ffc1d536f51522353dc24360d53a1838b73fbc57
chmaha <120390802+chmaha@users.noreply.github.com>
2024-12-12 22:11:34 +0000

    Fix check for SoX

--- truestereoconvert.sh
+++ truestereoconvert.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-if ! command -v sox &> /dev/null
+if ! command -v sox >/dev/null 2>&1
 then
     echo "Error: Please install SoX to use this script."
     echo "On Linux, you can install it with your package manager. For example:"
